6bb5be93f0db4cbc9b63b5f96fa7ed319a2f562f
Abdellah Aarab <105945475+abdellahaarab@users.noreply.github.com>
2022-09-30 02:38:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,17 @@ import pytupx as p
 print("Startiing...")
 
 BOT_TOKEN = config('token')
-
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-# updater = Updater(token=token, use_context=True)
-# app = updater.dispatcher
-
 #start
 app.add_handler(CommandHandler("start", m.start))
 #hello
 app.add_handler(CommandHandler("hello", m.hello))
 #help
 app.add_handler(CommandHandler("help", m.help))
-
 #ytb
 app.add_handler(MessageHandler(filters.TEXT  , p.ytb))
-   
-
-# app.add_handler(MessageHandler(Filters.text , ytb))
-
 
 app.run_polling()
+
+ # link="https://www.youtube.com/watch?v=PNYfcU5n0-4"
+   #by abdellah aarab
