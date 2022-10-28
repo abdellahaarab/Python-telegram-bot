99e8dead87fe8ec67901e86c1318f282de33557d
Abdellah Aarab <105945475+abdellahaarab@users.noreply.github.com>
2022-10-28 18:06:24 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+from decouple import config
+
+from telegram import Update
+from telegram.ext import * #ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters
+import fetch.api as api
+import methods as m 
+import pytupx as p
+
+print("Startiing...")
+
+BOT_TOKEN = config('token')
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+# updater = Updater(token=token, use_context=True)
+# app = updater.dispatcher
+
+#start
+app.add_handler(CommandHandler("start", m.start))
+#hello
+app.add_handler(CommandHandler("hello", m.hello))
+#help
+app.add_handler(CommandHandler("help", m.help))
+#anime
+app.add_handler(CommandHandler("anime", api.anime))
+#ytb
+app.add_handler(MessageHandler(filters.TEXT  , p.ytb))
+   
+
+# app.add_handler(MessageHandler(Filters.text , ytb))
+
+
+app.run_polling()
---
+++ pytupx.py
@@ -0,0 +1,61 @@
+# importing the module 
+from xml.dom.minidom import Document
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pytube import YouTube , Stream
+from telegram.ext import *
+from telegram import Update
+
+
+async def ytb(update: Update, context: ContextTypes.bot_data) -> None:
+    
+    # # link of the video to be downloaded 
+    # link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
+  
+    try: 
+        print(update.message.text)
+        data = YouTube(update.message.text)
+        
+        print('------------------------------------------------------')
+        print(data.author)
+        print('------------------------------------------------------')
+        print(data.streaming_data['formats'][2]["url"])
+        # Vidios1 = InlineKeyboardButton(text="Vidios", callback_data="v10")
+        # Audio1 = InlineKeyboardButton(text="Audio", callback_data="a10")
+        # kebord = InlineKeyboardMarkup().add(Vidios1 , Audio1)
+        
+        await update.message.reply_photo(data.thumbnail_url)
+        await update.message.reply_text(f"""\nTitle: {data.title} \nlength: {data.length} - Views: {data.views}""" )
+        # await update.message.reply_poll("Choose One ",["Vidios","Audio"], is_anonymous=True)
+        try:
+            await update.message.reply_video(data.streaming_data['formats'][2]["url"])
+        except:
+            await update.message.reply_text(data.streaming_data['formats'][1]["url"])
+
+        
+        # await update.edited_message()
+        # await update.message.reply_text(data.length)
+        # await update.message.reply_text(data.streaming_data['formats'][1]["url"])
+        # await update.message.reply_audio(data.streaming_data["adaptiveFormats"]["initRange"]["url"])
+        
+        print(f"thes is the id : {str(update.message.id)}")   
+    except:        
+        await update.message.reply_text("An error occurred, Try  https://tube.animexdrg.com/ !!") 
+    #     print("Connection Error") #to handle exception 
+  
+    # # filters out all the files with "mp4" extension 
+    # mp4files = yt.filter('mp4') 
+  
+    # #to set the name of the file
+    # yt.set_filename('GeeksforGeeks Video')
+      
+  
+    # # get the video with the extension and
+    # # resolution passed in the get() function 
+    # d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
+    # try: 
+    #     # downloading the video 
+    #     # d_video.download(SAVE_PATH)
+    #     await update.message.reply_text(f"Try contacting @amfih3bot ") 
+    # except: 
+    #     await update.message.reply_text(f"Try contacting @amfih3bot ")  
+ 
